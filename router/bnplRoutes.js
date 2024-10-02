dfa2a8b949286828c43d62af3d0db08dcdb62af8
Dylan Magalhaes <magalhaes.dylan@gmail.com>
2024-10-02 10:18:47 +0200

    clean router bnpl

--- router/bnplRoutes.js
+++ router/bnplRoutes.js
@@ -3,23 +3,23 @@ const router = express.Router();
 const bnplController = require("../controllers/bnplController");
 
 // Route pour la simulation BNPL
-router.post("/bnpl/simulate", bnplController.simulateBNPL);
+router.post("/simulate", bnplController.simulateBNPL);
 
 // Créer une nouvelle vente BNPL
-router.post("/bnpl/sale", bnplController.createBNPLSale);
+router.post("/sale", bnplController.createBNPLSale);
 
 // Payer une mensualité BNPL
-router.post("/bnpl/payment", bnplController.payBNPLInstallment);
+router.post("/payment", bnplController.payBNPLInstallment);
 
 // Récupérer les détails d'une vente BNPL
-router.get("/bnpl/sale/:saleId", bnplController.getBNPLSaleDetails);
+router.get("/sale/:saleId", bnplController.getBNPLSaleDetails);
 
 // Récupérer toutes les ventes BNPL pour un utilisateur
-router.get("/bnpl/user/:userPubKey/sales", bnplController.getUserBNPLSales);
+router.get("/user/:userPubKey/sales", bnplController.getUserBNPLSales);
 
 // Télécharger l’échéancier
 router.get(
-  "/bnpl/sale/:saleId/schedule/:type/pdf",
+  "/sale/:saleId/schedule/:type/pdf",
   bnplController.downloadSchedulePDF
 );
 
--- scheduler.js
+++ scheduler.js
@@ -1,15 +1,15 @@
-const cron = require('node-cron');
-const bnplService = require('./services/bnplServices');
+const cron = require("node-cron");
+const bnplService = require("./services/bnplServices");
 
 // Planifier la tâche pour qu'elle s'exécute tous les jours à minuit
-cron.schedule('0 0 * * *', async () => {
-    console.log('Vérification des paiements BNPL dus...');
+cron.schedule("* * * * *", async () => {
+  console.log("Vérification des paiements BNPL dus...");
 
-    try {
-        // Appeler la méthode pour vérifier et traiter les paiements
-        await bnplService.schedulePayments();
-        console.log('Paiements BNPL vérifiés et traités avec succès.');
-    } catch (error) {
-        console.error('Erreur lors de la vérification des paiements BNPL :', error);
-    }
+  try {
+    // Appeler la méthode pour vérifier et traiter les paiements
+    await bnplService.schedulePayments();
+    console.log("Paiements BNPL vérifiés et traités avec succès.");
+  } catch (error) {
+    console.error("Erreur lors de la vérification des paiements BNPL :", error);
+  }
 });
