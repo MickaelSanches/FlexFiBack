7281ad1ce6d44b0b52ff6dcc14e1c53aafde84bf
Dylan Magalhaes <magalhaes.dylan@gmail.com>
2024-10-28 23:38:12 +0100

    fix nodemailer

--- services/userService.js
+++ services/userService.js
@@ -70,7 +70,7 @@ class UserService {
     const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: process.env.EMAIL_PORT,
-      secure: true,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: String(process.env.EMAIL_PASS),
