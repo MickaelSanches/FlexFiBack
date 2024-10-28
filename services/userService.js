2ac95c2108e8ba447e63f69c444ae3b08981942a
Dylan Magalhaes <magalhaes.dylan@gmail.com>
2024-10-28 22:29:43 +0100

    password nodemailer to string

--- services/userService.js
+++ services/userService.js
@@ -73,7 +73,7 @@ class UserService {
       secure: false,
       auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        pass: String(process.env.EMAIL_PASS),
       },
     });
 
