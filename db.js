846f4d34a0ade7679a2e15666abaab40f203ba52
Dylan Magalhaes <magalhaes.dylan@gmail.com>
2024-10-29 14:03:20 +0100

    fix db

--- db.js
+++ db.js
@@ -1,16 +1,17 @@
-const { Pool } = require('pg');
-require('dotenv').config();
+const { Pool } = require("pg");
+require("dotenv").config();
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASS,
-    port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: "LesYeuxVerslAvenir",
+  port: process.env.DB_PORT,
 });
 
-pool.connect()
-    .then(() => console.log('Connected to PostgreSQL'))
-    .catch(err => console.log(err));
+pool
+  .connect()
+  .then(() => console.log("Connected to PostgreSQL"))
+  .catch((err) => console.log(err));
 
 module.exports = pool;
--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
+  password: "LesYeuxVerslAvenir",
   port: process.env.DB_PORT || 5000,
 });
 
